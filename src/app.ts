d2082678efac1ef85f4cc48bc22456801c09c0fc
iamraselmolla <raselmolla6336@gmail.com>
2026-09-08 15:54:24 +0600

    fixed the vercel issues

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,11 @@ export function createApp(): Express {
   const app = express();
 
   app.use(
-    cors({
-      origin: env.corsOrigins,
-      credentials: true,
-    })
-  );
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true }));
 
